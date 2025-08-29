5059911bbf7aeb3806cb4b32793db39648bdd9d9
Inx51 <andrenmattias@gmail.com>
2025-08-30 00:28:02 +0200

    reset

--- .devcontainer/setup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-# Create a directory for storage when using the file system storage in development.
-mkdir -p /tmp/howlite-resources-storage/
-
-go install github.com/air-verse/air@latest
-cd /workspaces/howlite-resources-v3/src
-go mod tidy
--- README.md
+++
@@ -1,2 +0,0 @@
-# ⚠️ **Still in development!**
-Application for quickly creating basic CRUD APIs that allows storage and retrieval of any generic resources over HTTP.
